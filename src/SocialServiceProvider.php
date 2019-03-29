6729033710cfad55c5ffc2a4a71596fd874fcb6f
Igor Gawryś <igorgawrys@social.local>
2019-03-29 19:41:05 +0100

    Repair Finished

--- src/SocialServiceProvider.php
+++ src/SocialServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Igorgawrys\Social;
 
+use Auth;
+use Hautelook\Phpass\PasswordHash;
 use Illuminate\Support\ServiceProvider;
 use Igorgawrys\Social\Auth\EloquentSocialUserProvider;
 use Igorgawrys\Social\Guard\SocialGuard;
@@ -9,6 +11,13 @@ use Igorgawrys\Social\Hashing\SocialHasher;
 
 class SocialServiceProvider extends ServiceProvider
 {
+     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
     /**
      * Bootstrap services.
      *
@@ -20,7 +29,7 @@ class SocialServiceProvider extends ServiceProvider
             __DIR__ . '/../config/social.php' => config_path('social.php'),
         ]);
     }
-    
+
     /**
      * Register services.
      *
@@ -29,19 +38,32 @@ class SocialServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/social.php', 'social');
+
         Auth::extend('social', function ($app) {
             return new SocialGuard(
                 new EloquentSocialUserProvider($app['social'], $config['model'])
             );
         });
+
         $this->app->singleton('social-auth', function ($app) {
             $iteration_count = $app['config']->get('social-auth.hash.iteration_count');
             $portable_hashes = $app['config']->get('social-auth.hash.portable_hashes');
             $hasher = new PasswordHash($iteration_count, $portable_hashes);
             return new SocialHasher($hasher);
         });
+
         Auth::provider('eloquent.social', function ($app, array $config) {
             return new EloquentSocialUserProvider($app['social'], $config['model']);
         });
+    }
+
+       /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides()
+    {
+        return ['social'];
     }
 }
